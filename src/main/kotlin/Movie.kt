8224aaef6fe7656653fd926641bd4f6cd401de89
Hugo Vallada <valladahugo@gmail.com>
2023-05-14 14:33:59 -0300

    post-detekt: Ajustes de código

--- src/main/kotlin/Movie.kt
+++ src/main/kotlin/Movie.kt
@@ -1,30 +1,34 @@
+const val MAX_SIZE_OF_SHORT_FILMS = 60
+const val MINIMUM_AGE_FOR_R_MOVIES = 18
+
 data class Movie(
     val name: String,
     val director: String,
-    private val Cast: MutableSet<String> = mutableSetOf(),
+    private val cast: MutableSet<String> = mutableSetOf(),
     val length: Int,
     var rate: Char
 ) {
-    val classification: String = if (length in 1..60) {
+    val classification: String = if (length in 1..MAX_SIZE_OF_SHORT_FILMS) {
         "short"
     } else {
         "long"
     }
 
     fun addToCast(vararg name: String) {
-        Cast.addAll(name)
+        cast.addAll(name)
     }
 
-    fun seeCast(): Set<String> = Cast
+    fun seeCast(): Set<String> = cast
 
     fun isAllowedToSee(age: Int) {
-        if ((rate == 'R') and (age < 18)) {
+        if ((rate == 'R') and (age < MINIMUM_AGE_FOR_R_MOVIES)) {
             println("You are not allowed too see!")
         } else {
             println("You are allowed too see!")
         }
     }
 
-    fun changeRate(){}
-
+    fun changeRate(newRate: Char) {
+        rate = newRate
+    }
 }
